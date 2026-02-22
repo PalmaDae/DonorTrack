08ddd0e0ed3fe081cf0faf68dc5768df04281e4f
palmadae <arturd277@mail.ru>
2026-02-22 17:39:53 +0300

    Random Fact

--- app/src/main/java/com/example/donortrack/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/donortrack/ui/screen/MainScreen.kt
@@ -1,18 +1,39 @@
 package com.example.donortrack.ui.screen
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.donortrack.R
 import com.example.donortrack.ui.theme.DonorTrackTheme
 
 @Composable
 fun MainApp() {
-    FactCard(fact = R.string.fact1)
+    val items = stringArrayResource(id = R.array.facts_quotes)
+
+    val randomItem = items.random()
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item { FactCard(fact = randomItem) }
+    }
 }
 
 
@@ -36,14 +57,24 @@ fun MainScreenPreviewDark() {
 
 
 
+
 @Composable
 fun FactCard(
     modifier: Modifier = Modifier,
-    @StringRes fact: Int
+    fact: String
 ) {
-    Card {
-        Text(
-            text = stringResource(fact)
-        )
+    Card(
+        modifier = modifier
+            .size(width = (250.dp), height = 85.dp),
+        shape = RoundedCornerShape(8.dp),
+    ) {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = fact,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
