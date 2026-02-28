1cb17dc801f986cd4b48036f2a6eadd9556598ce
palmadae <arturd277@mail.ru>
2026-02-28 16:52:41 +0300

    getstations

--- app/src/main/java/com/example/donortrack/network/api/DonorAPI.kt
+++ app/src/main/java/com/example/donortrack/network/api/DonorAPI.kt
@@ -1,5 +1,6 @@
 package com.example.donortrack.network.api
 
+import com.example.donortrack.network.model.City
 import com.example.donortrack.network.model.CityList
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,4 +11,9 @@ interface DonorAPI {
         @Query("all_bs") allBs: Boolean = true,
         @Query("country") country: Int = 1
     ) : CityList
+
+    @GET("blood_stations/")
+    suspend fun getStations(
+        @Query("city_id") cityId: Int
+    )
 }
