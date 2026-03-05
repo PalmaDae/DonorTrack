bcd6ce98a5071f1b7c2ed2ac219f7a8e7944c98d
PalmaDae <palmadae@RedmiBook.localhost>
2026-03-05 16:41:33 +0300

    retrofit 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,6 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
 
     implementation("com.google.code.gson:gson:2.8.7")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
 }
