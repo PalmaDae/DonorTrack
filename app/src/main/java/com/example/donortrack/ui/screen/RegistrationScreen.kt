f9f6221e1f0d8509450e3b77e6c269c848f11bc9
PalmaDae <arturd277@mail.ru>
2026-03-05 17:01:05 +0300

    reg

--- app/src/main/java/com/example/donortrack/ui/screen/RegistrationScreen.kt
+++ app/src/main/java/com/example/donortrack/ui/screen/RegistrationScreen.kt
@@ -1,2 +1,39 @@
 package com.example.donortrack.ui.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.donortrack.ui.theme.DonorTrackTheme
+
+@Composable
+fun RegisterApp() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+@Composable
+@Preview
+fun RegisterPreview() {
+    DonorTrackTheme {
+        RegisterApp()
+    }
+}
+
+
+@Composable
+@Preview
+fun RegisterPreviewDark() {
+    DonorTrackTheme(
+        darkTheme = true
+    ) {
+        RegisterApp()
+    }
+}
