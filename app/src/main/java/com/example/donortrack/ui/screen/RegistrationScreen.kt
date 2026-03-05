f044b75088e59dd565262a946c12afa9e36cb749
PalmaDae <arturd277@mail.ru>
2026-03-05 17:27:57 +0300

    outlinedtext fields

--- app/src/main/java/com/example/donortrack/ui/screen/RegistrationScreen.kt
+++ app/src/main/java/com/example/donortrack/ui/screen/RegistrationScreen.kt
@@ -1,12 +1,23 @@
 package com.example.donortrack.ui.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.donortrack.R
 import com.example.donortrack.ui.theme.DonorTrackTheme
 
 @Composable
 fun RegisterApp() {
-
+    InputFields()
 }
 
 
@@ -14,6 +25,37 @@ fun RegisterApp() {
 
 
 
+@Composable
+fun InputFields(
+    modifier: Modifier = Modifier
+) {
+    var email by remember { mutableStateOf("") }
+    var login by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(stringResource(R.string.email)) },
+            placeholder = { Text(stringResource(R.string.exampleEmail)) }
+        )
+
+        OutlinedTextField(
+            value = login,
+            onValueChange = { login = it },
+            label = { Text(stringResource(R.string.username)) },
+            placeholder = { Text(stringResource(R.string.exampleUsername)) }
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(stringResource(R.string.password)) },
+            placeholder = { Text(stringResource(R.string.examplePassword)) }
+        )
+    }
+}
 
 
 
